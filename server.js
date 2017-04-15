b282e88837d1c9fee1fbf4ae7f033a49d2f93824
029281 <sheilayuan@C02T511YHF1T.home>
2017-04-14 20:52:45 -0400

    Initial setup, server working, connected to db

---
+++ server.js
@@ -0,0 +1,38 @@
+// require package
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+
+// middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+// session middleware
+
+// route controller
+
+
+// Mongoose Db
+mongoose.connect('mongodb://localhost:27017/danceportfolio');
+mongoose.connection.once('open', function(){
+  console.log('Project 2 Dance Portfolio connect to mongo db');
+});
+
+// default (login page)
+app.get('/', function(req,res){
+  res.render('index.ejs');
+  // for later session
+  // res.render('index.ejs', {
+  //   currentUser: req.session.currentuser
+  // });
+});
+
+
+
+
+app.listen(3000, function(){
+  console.log('Project 2 Dance Portfolio Listen');
+});
